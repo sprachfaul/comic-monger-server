ada56931e81b74962b92c69ca43aa2f687c1bfc2
sprachfaul <shalfter@gmail.com>
2019-12-24 08:03:06 +0000

    + project now a webapp

--- comic-monger-server/src/main/java/de/sprachfaul/comic/monger/server/ComicMongerApplication.java
+++ comic-monger-server/src/main/java/de/sprachfaul/comic/monger/server/ComicMongerApplication.java
@@ -3,6 +3,7 @@ package de.sprachfaul.comic.monger.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 import de.sprachfaul.comic.monger.server.controller.ControllerConfig;
@@ -13,7 +14,7 @@ import de.sprachfaul.comic.monger.server.services.ComicService;
 @SpringBootApplication
 @Import({ControllerConfig.class, SpringDataRepositoryRestMvcConfiguration.class, JPAConfig.class, ComicService.class})
 @EnableAutoConfiguration
-public class ComicMongerApplication {
+public class ComicMongerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ComicMongerApplication.class, args);
